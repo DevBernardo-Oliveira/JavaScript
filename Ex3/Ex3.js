7545c02c9118108ddd6c29eb88ba29faf6625201
Bernardo-De-Oliveira-Ribeiro <bifcddiw@gmail.com>
2025-01-28 16:16:44 -0300

    Add files via upload

---
+++ Ex3/Ex3.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite seu nome: ")
+let senha = prompt("Digite sua senha: ")
+let nivel = prompt("Digite seu nivel: ")
+
+if((nome=="Bernardo")&&(senha==12)&&(nivel==2)){
+    console.log("Acesso Liberado")
+}else{
+    console.log("Acesso negado")
+}
